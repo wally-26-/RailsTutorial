5a513b20d1ae6a707d594a7e2c1ef3b5b31e901d
Andrea Carron <deksmail@gmail.com>
2015-03-20 15:21:12 +0100

    Embedded Ruby + ex 1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,32 @@
 require 'spec_helper' 
 
 describe "Static pages" do
+   
     describe "Home page" do
-        it "should have the content 'Sample App'" do 
+        it "should have the title 'Home'" do 
           visit '/static_pages/home'
-          page.should have_content('Sample App')
+          page.should have_title("Home")
         end 
+    end
+    
+    describe "Help page" do
+        it "should have the title 'Help '" do 
+          visit '/static_pages/help'
+          page.should have_title('Help')
+        end 
+    end   
+    
+    describe "About page" do
+        it "should have the h1 'About Us'" do 
+          visit '/static_pages/about'
+          page.should have_selector('h1', :text => 'About Us')
+        end 
+    end   
+    
+    describe "Contacts Page" do
+       it "should have the title 'Contacts'" do
+         visit '/static_pages/contacts'
+         page.should have_title('Contacts')
+       end
     end
 end
