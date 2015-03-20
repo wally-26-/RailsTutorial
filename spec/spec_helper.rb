d3b64d477ff65c286fe3811a98ceb64edd5b5990
Andrea Carron <deksmail@gmail.com>
2015-03-20 12:37:23 +0100

    Fixed rspec in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,34 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+
+## Added due to suggestion
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+#################
+
+
 RSpec.configure do |config|
+  
+   ######Added
+   config.include Capybara::DSL
+
+   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+   config.use_transactional_fixtures = true
+
+   config.infer_base_class_for_anonymous_controllers = false
+
+   config.order = "random"
+   ##########
+   
+   
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
