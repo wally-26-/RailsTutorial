9e0a1f1ac1abfb1e4c60cbed7c8e9fb01decae99
Andrea Carron <deksmail@gmail.com>
2015-03-23 15:28:42 +0100

    More pretty test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,8 +6,8 @@ describe "Users pages" do
     
     describe "Sign Up page" do
        before { visit signup_path }
-       it { should have_selector('h1', text: 'Sign up') } 
-       it { should have_title(full_title('Sign up')) } 
+       it { should have_selector('h1', text: 'Sign Up') } 
+       it { should have_title(full_title('Sign Up')) } 
     end
 end
   
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 require 'capybara'
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
